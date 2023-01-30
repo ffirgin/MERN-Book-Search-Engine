276ba224d49f5b2099673181e260dedb9d719816
Griffin <ffirgn@gmail.com>
2023-01-29 16:57:42 -0800

    mutations setup to run login_user, add_user, save_book, remove_book to execute each mutation.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { authMiddleware } = require('./utils/auth');
+
+const { ApolloServer } = require('apollo-server-express');
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
