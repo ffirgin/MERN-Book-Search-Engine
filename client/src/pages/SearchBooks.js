870a45decee41d4b711a44f0eec6428df19f0147
Griffin <ffirgn@gmail.com>
2023-01-29 17:37:16 -0800

    fixed issue in searchbook file

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -4,7 +4,6 @@ import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -15,6 +14,7 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -68,7 +68,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook({ variables: { bookData: ...bookToSave } });
+      const {data} = await saveBook({ variables: { bookData: {...bookToSave} } });
       console.log(savedBookIds)
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
